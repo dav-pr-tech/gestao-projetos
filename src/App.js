899bb1bc80541b9775fa3d936fdf40ca05e78edb
David Gonçalves <david.a.p.g@hotmail.com>
2024-07-05 18:05:56 -0300

    gh - page

--- src/App.js
+++ src/App.js
@@ -12,8 +12,15 @@ function App() {
           <button onClick={() => window.location.href='https://dav-pr-tech.github.io/projeto-tap/'}>Termo de Abertura do Projeto – TAP</button>
           <button onClick={() => window.location.href='https://dav-pr-tech.github.io/projeto-escopo/'}>Escopo</button>
           <button onClick={() => window.location.href='https://dav-pr-tech.github.io/projeto-checklist/'}>Checklist</button>
-          <button onClick={() => window.location.href='http://example.com'}>Benefícios-resultados</button>
-          <button onClick={() => window.location.href='http://example.com'}>EAP</button>
+          <button onClick={() => window.location.href='https://dav-pr-tech.github.io/projeto-beneficios-resultados/'}>Benefícios-resultados</button>
+          <button onClick={() => window.location.href='https://davg505.github.io/cronograma-projeto/'}>EAP-Cronograma</button>
+          <button onClick={() => window.location.href='https://dav-pr-tech.github.io/projeto-v-m-v/'}>Visão Missão e Valores </button>
+          <button onClick={() => window.location.href='https://davg505.github.io/financial-indicators/'}>Análise de Viabilidade Financeira </button>
+          <button onClick={() => window.location.href='https://davg505.github.io/risk-management/'}>Gerenciamento de Riscos </button>
+          <button onClick={() => window.location.href='https://dav-pr-tech.github.io/gestao-qualidade-projetos/'}>Gestão da Qualidade em Projetos</button>
+          <button onClick={() => window.location.href='https://dav-pr-tech.github.io/gerenciamento-recursos/'}>Gerenciamento de Recursos</button>
+          <button onClick={() => window.location.href='https://dav-pr-tech.github.io/projeto-gerenciamento-comunicacao/'}>Gerenciamento de Comunicação em Projetos</button>
+          <button onClick={() => window.location.href='https://dav-pr-tech.github.io/sistema-aquisicoes/'}>Gerenciamento das Aquisições</button>
           {/* Adicione mais botões conforme necessário */}
         </div>
       </main>
